378f5e161331a0d5cc391b12f75b53895af0e888
anharfathoni <anharaf28@gmail.com>
2018-11-07 18:48:38 +0700

    Main Program

--- todo.js
+++ todo.js
@@ -1,14 +1,5 @@
 const argv = process.argv.slice(2)
-// console.log(argv)
-// const splitArgv = argv[0].split(":")
-
-// const command = splitArgv[0]
-// const command2 = splitArgv[1]
 const option = argv.slice(1)
-// console.log(option)
-// console.log("//",command)
-// console.log(",,,,",command2)
-// console.log("???",option)
 
 const Controller =  require("./Controllers/Controller.js")
 
