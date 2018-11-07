0bfa33b08964f08d08b6e310615d5aec8dba8745
patriagani <patria.gani@gmail.com>
2018-11-07 19:59:17 +0700

    js todos done

--- todo.js
+++ todo.js
@@ -1,1 +1,14 @@
+const Controller = require('./Controllers/Controller.js');
 
+let argv = process.argv.slice(2)
+
+let command = argv[0]
+let options = argv.slice(1)
+
+if (command.match("filter:")) {
+  let commandfilter = command.split(":")
+  command = commandfilter[0]
+  options = commandfilter[1]
+}
+
+Controller.execute(command, options)
